2729be3c66556744b3abb0d16fbd679c34799651
aovando <aovandodevsxr@gmail.com>
2024-09-16 03:41:03 -0400

    player controller + focal point

---
+++ Prototype_4/Assets/Scripts/RotateCamera.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateCamera : MonoBehaviour
+{
+    //public GameObject cam;
+    public float speed = 25.0f; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+
+        transform.Rotate(Vector3.up, horizontalInput * speed * Time.deltaTime); 
+    }
+}
