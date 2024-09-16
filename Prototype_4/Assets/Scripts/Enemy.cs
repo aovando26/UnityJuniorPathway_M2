93432e805f646064dbecd62c0bd200d2ff499135
aovando <aovandodevsxr@gmail.com>
2024-09-16 18:49:18 -0400

    coroutines and ienumerators

--- Prototype_4/Assets/Scripts/Enemy.cs
+++ Prototype_4/Assets/Scripts/Enemy.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     private Rigidbody _rigidbody;
-    public GameObject _player;
+    private GameObject _player;
     public float speed = 2.5f;
 
     // Start is called before the first frame update
--- Prototype_4/Assets/Scripts/PlayerController.cs
+++ Prototype_4/Assets/Scripts/PlayerController.cs
@@ -8,6 +8,13 @@ public class PlayerController : MonoBehaviour
     private Rigidbody _rigidbody;
     private GameObject focalPoint;
 
+    public GameObject powerUpIndicator;
+
+    private bool hasPowerUp = false;
+    private float powerUpStrength = 10.0f;
+
+    // countdown timer
+    //private float countdownTimer = 5.0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -22,5 +29,47 @@ public class PlayerController : MonoBehaviour
 
         _rigidbody.AddForce(focalPoint.transform.forward * verticalInput * speed);
         //transform.Translate(Vector3.right * verticalInput * speed * Time.deltaTime);
+        powerUpIndicator.transform.position = transform.position + new Vector3 (0, -0.5f, 0);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Pickups"))
+        {
+            powerUpIndicator.gameObject.SetActive(true);
+            hasPowerUp = true;
+            Destroy(other.gameObject);
+            StartCoroutine(PowerDown());
+        }
+    }
+
+    private void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.CompareTag("Enemy") && hasPowerUp)
+        {
+            // get rigidbody 
+            Rigidbody enemyRb = col.gameObject.GetComponent<Rigidbody>(); 
+            
+            // find difference 
+            Vector3 awayFromPlayer = col.gameObject.transform.position - transform.position;
+
+            // add force 
+            enemyRb.AddForce(awayFromPlayer * powerUpStrength, ForceMode.Impulse); 
+            Debug.Log("Collider with " + col.gameObject.name + "power up is " + hasPowerUp);
+
+            //countdownTimer -= Time.deltaTime;
+
+            //if (countdownTimer <= 0)
+            //{
+                
+            //}
+        }
+    }
+
+    IEnumerator PowerDown()
+    {
+        yield return new WaitForSeconds(5.0f);
+        hasPowerUp = false;
+        powerUpIndicator.gameObject.SetActive(false);
     }
 }
