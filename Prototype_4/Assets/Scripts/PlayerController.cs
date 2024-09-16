6ad2d091d16f2b8813742d7ae734b2f9bdd838d9
aovando <aovandodevsxr@gmail.com>
2024-09-16 03:41:03 -0400

    player controller + focal point

---
+++ Prototype_4/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 5.0f;
+    private Rigidbody _rigidbody;
+    public GameObject focalPoint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+        focalPoint = GameObject.Find("Focal Point");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float verticalInput = Input.GetAxis("Vertical");
+
+        _rigidbody.AddForce(focalPoint.transform.forward * verticalInput * speed);
+        //transform.Translate(Vector3.right * verticalInput * speed * Time.deltaTime);
+    }
+}
