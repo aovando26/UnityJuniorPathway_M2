793b806c7e7d904d5b8c9fbb53ae32dd10b3d3c9
aovando <aovandodevsxr@gmail.com>
2024-09-16 06:02:42 -0400

    spawn manager script

---
+++ Prototype_4/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    // gameobject
+    public GameObject enemies;
+    private float startDelay = 2.5f;
+    private float repeatRate = 1.5f;
+    private float spawnRange = 6.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnEnemies", startDelay, repeatRate);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //SpawnEnemies();
+    }
+
+    private void SpawnEnemies() 
+    {
+        float randomPos = Random.Range(-spawnRange, spawnRange);
+
+        Vector3 spawnPos = new Vector3(randomPos, 0, randomPos);
+
+        Instantiate(enemies, spawnPos, enemies.transform.rotation);
+    }
+}
