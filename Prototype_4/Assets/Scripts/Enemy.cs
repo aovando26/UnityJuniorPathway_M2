addb778a3f501b1c38c406a502eb81ec1835a84a
aovando <aovandodevsxr@gmail.com>
2024-09-16 05:14:53 -0400

    physics materials added + private gameobject

---
+++ Prototype_4/Assets/Scripts/Enemy.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    private Rigidbody _rigidbody;
+    public GameObject _player; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+        _player = GameObject.Find("Player");
+
+        if (_player == null)
+        {
+            Debug.LogWarning("No player found");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _rigidbody.AddForce(_player.transform.forward);
+    }
+}
