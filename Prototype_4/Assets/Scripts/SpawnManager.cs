10b0cbdf094b3cbd058ff9801d82550a2757bcaa
aovando <aovandodevsxr@gmail.com>
2024-09-16 06:05:59 -0400

    method with return values

--- Prototype_4/Assets/Scripts/SpawnManager.cs
+++ Prototype_4/Assets/Scripts/SpawnManager.cs
@@ -6,13 +6,14 @@ public class SpawnManager : MonoBehaviour
 {
     // gameobject
     public GameObject enemies;
-    private float startDelay = 2.5f;
-    private float repeatRate = 1.5f;
+    //private float startDelay = 2.5f;
+    //private float repeatRate = 1.5f;
     private float spawnRange = 6.0f;
     // Start is called before the first frame update
     void Start()
     {
-        InvokeRepeating("SpawnEnemies", startDelay, repeatRate);
+        //InvokeRepeating("SpawnEnemies", startDelay, repeatRate);
+        Instantiate(enemies, SpawnEnemies(), enemies.transform.rotation);
     }
 
     // Update is called once per frame
@@ -21,12 +22,12 @@ public class SpawnManager : MonoBehaviour
         //SpawnEnemies();
     }
 
-    private void SpawnEnemies() 
+    private Vector3 SpawnEnemies() 
     {
         float randomPos = Random.Range(-spawnRange, spawnRange);
 
         Vector3 spawnPos = new Vector3(randomPos, 0, randomPos);
 
-        Instantiate(enemies, spawnPos, enemies.transform.rotation);
+        return spawnPos;
     }
 }
