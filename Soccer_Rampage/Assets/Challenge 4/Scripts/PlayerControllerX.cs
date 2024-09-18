b7f0a461a68cd67d86dddf46f35937609d503867
aovando <aovandodevsxr@gmail.com>
2024-09-18 11:13:13 -0400

    unity learn challenge

---
+++ Soccer_Rampage/Assets/Challenge 4/Scripts/PlayerControllerX.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    private Rigidbody playerRb;
+    private float speed = 500;
+    private GameObject focalPoint;
+
+    public bool hasPowerup;
+    public GameObject powerupIndicator;
+    public int powerUpDuration = 5;
+
+    private float normalStrength = 10; // how hard to hit enemy without powerup
+    private float powerupStrength = 25; // how hard to hit enemy with powerup
+    private float cooldownTimer;
+    public ParticleSystem smokeParticle;
+
+    // offset vectors 
+    Vector3 offset = new Vector3(0f, -0.55f, 0f);
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        focalPoint = GameObject.Find("Focal Point");
+    }
+
+    void Update()
+    {
+        // Add force to player in direction of the focal point (and camera)
+        float verticalInput = Input.GetAxis("Vertical");
+        playerRb.AddForce(focalPoint.transform.forward * verticalInput * speed * Time.deltaTime); 
+
+        // Set powerup indicator position to beneath player
+        powerupIndicator.transform.position = transform.position + new Vector3(0, -0.6f, 0);
+        smokeParticle.transform.position = transform.position + offset;
+
+        PlayerTurboBoost();
+    }
+
+    // the player will get a turbo  boost when player presses spacebar
+    private void PlayerTurboBoost()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // Debug.Log("Space key was pressed");
+            playerRb.AddForce(focalPoint.transform.forward * 10.0f, ForceMode.Impulse);
+
+            // activate particle effect + offset its vector to follow player
+            smokeParticle.Play();
+        }
+    }
+        
+    // If Player collides with powerup, activate powerup
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Powerup"))
+        {
+            Destroy(other.gameObject);
+            // Debug.Log("Object has been destroyed: " + other.gameObject.name);
+            hasPowerup = true;
+            powerupIndicator.SetActive(true);
+            StartCoroutine(PowerupCooldown());
+        }
+    }
+
+    // Coroutine to count down powerup duration
+    IEnumerator PowerupCooldown()
+    {
+        yield return new WaitForSeconds(powerUpDuration);
+        hasPowerup = false;
+        powerupIndicator.SetActive(false);
+    }
+
+    // If Player collides with enemy
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            Rigidbody enemyRigidbody = other.gameObject.GetComponent<Rigidbody>();
+
+            // hitting an enemy ends it away from player
+            Vector3 awayFromPlayer =  other.gameObject.transform.position - transform.position;
+
+            if (hasPowerup) // if have powerup hit enemy with powerup force
+            {
+                enemyRigidbody.AddForce(awayFromPlayer * powerupStrength, ForceMode.Impulse);
+            }
+            else // if no powerup, hit enemy with normal strength 
+            {
+                enemyRigidbody.AddForce(awayFromPlayer * normalStrength, ForceMode.Impulse);
+            }
+
+
+        }
+    }
+}
