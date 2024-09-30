3b83f3d8cfbf636c27ee0429c67df458f78cd958
aovando <aovandodevsxr@gmail.com>
2024-09-30 15:10:07 -0400

    score manager + ui

--- Prototype_5/Assets/Scripts/GameManager.cs
+++ Prototype_5/Assets/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro; 
 
 public class GameManager : MonoBehaviour
 {
@@ -8,16 +9,23 @@ public class GameManager : MonoBehaviour
 
     private float spawnRate = 3.0f; 
 
+    // ui variables
+    private int score;
+    public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI gameOverText;
+
     // Start is called before the first frame update
     void Start()
     {
         StartCoroutine(SpawnTargets());
+
+        score = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     IEnumerator SpawnTargets()
@@ -28,5 +36,16 @@ public class GameManager : MonoBehaviour
             int indexCount = Random.Range(0, targets.Count);
             Instantiate(targets[indexCount]);   /*, transform.position, targets[indexCount].transform.rotation);*/
         }
+    }
+
+    public void UpdateScore(int scoreToAdd)
+    {
+        score += scoreToAdd;
+        scoreText.text = "Score " + score;
+    }
+
+    public void GameOver()
+    {
+        gameOverText.gameObject.SetActive(true);
     }
 }
