e90c3a790cb291061ab78e67776507c0ff98f1bf
aovando <aovandodevsxr@gmail.com>
2024-09-30 18:59:17 -0400

    scene management, restart + gameover

--- Prototype_5/Assets/Scripts/Target.cs
+++ Prototype_5/Assets/Scripts/Target.cs
@@ -49,9 +49,12 @@ public class Target : MonoBehaviour
 
     private void OnMouseDown()
     {
-        Destroy(gameObject);
-        gameManager.UpdateScore(pointValue);
-        Instantiate(explosionParticle, transform.position, transform.rotation);
+        if (gameManager.isGameActive)
+        {
+            Destroy(gameObject);
+            gameManager.UpdateScore(pointValue);
+            Instantiate(explosionParticle, transform.position, transform.rotation);
+        }
     }
 
     private void OnTriggerEnter(Collider col)
